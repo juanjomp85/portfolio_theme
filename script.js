66395cbda245db68efe440fb5664cb60a3d78854
juanjomp85 <juanjomp85@gmail.com>
2024-06-07 16:50:15 +0200

    6th

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(window).scroll(function() {
       $('.portfolio-item').each(function(i) {
-          var bottom_of_object = $(this).position().top + $(this).outerHeight();
+          var top_of_object = $(this).position().top; //+ $(this).outerHeight();
           var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          if (bottom_of_window > bottom_of_object) {
-              $(this).animate({'opacity': '1'}, 1000);
+          if (bottom_of_window > top_of_object) {
+              $(this).animate({'opacity': '1'}, 500);
           }
       });
   });
